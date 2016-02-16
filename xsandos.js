487b83b59d579ffc471143e5bfdd48a12e8cc972
steveoneil <steve@steveoneil.com>
2016-02-15 21:24:18 -0500

    Add css animations

--- xsandos.js
+++ xsandos.js
@@ -14,61 +14,63 @@ $(document).on('ready', function() {
 
   playGame();
 
-    // var playAgain;
+  function playGame () {
 
-    function playGame () {
+    // Object that keeps each player's score of the number of "tokens" collected for each path
+    var score = { 'X': [0, 0, 0, 0, 0, 0, 0, 0],
+                  'O': [0, 0, 0, 0, 0, 0, 0, 0]};
+    var turn = 0;
 
-      // Object that keeps each player's score of the number of "tokens" collected for each path
-      var score = { 'X': [0, 0, 0, 0, 0, 0, 0, 0],
-                    'O': [0, 0, 0, 0, 0, 0, 0, 0]};
+    $('td').html('');
 
-      var turn = 0;
+    $('td').one('click', function() {
 
+      var self = $(this);
+      var selection = self.attr('id');
+      var player = '';
 
-      $('td').on('click', function() {
+      turn % 2 ? player = 'O' : player = 'X';
 
-        var self = $(this);
-        var selection = self.attr('id');
-        var player = '';
+      // self.css('opacity', 1);
+      self.html(player).css('opacity', 1).animate({
+        fontSize: '300',
+      }, 500);
+      self.animate({
+        opacity: 0.50,
+        fontSize: '200',
+      }, 300);
 
-        turn % 2 ? player = 'O' : player = 'X';
+      addScore(player, selection);
 
-        self.html(player);
-        self.off('click');
+      if ( checkForWinner(player) ) {
+        alert("Player " + player + " WINS!    Press \"Enter\" to play again.");
+        reset();
+      } else if (turn >= 8) {
+        alert("Draw....No Winner!    Press \"Enter\" to play again.");
+        reset();
+      }
+
+      turn++;
+
+    });
 
     // Add tokens (for each path) to players score array
-        for (var i = 0; i < 7; i++) {
-          score[player][i] = score[player][i] + tokenMap[selection][i];
-        }
-
-        if ( checkForWinner(player) ) {
-          alert("Player " + player + " WINS!");
-          reset();
-        } else if (turn >= 8) {
-            alert("Draw....No Winner!");
-            reset();
-        }
-
-        turn++;
-
-      });
-
-      function checkForWinner(player) {
-        return !score[player].every(element => element < 4);
+    function addScore(player, selection) {
+      for (var i = 0; i < 7; i++) {
+        score[player][i] = score[player][i] + tokenMap[selection][i];
       }
-
-      function reset() {
-        $('td').off('click');
-        $('td').html('');
-        // playAgain = true;
-        // return playAgain;
-        playGame();
-      }
-
     }
 
-  // if (playAgain) {
-  //   playGame();
-  // }
+    function checkForWinner(player) {
+      return !score[player].every(element => element < 4);
+    }
+
+    function reset() {
+      $('td').off('click');
+      // $('td').css('opacity' 1);
+      playGame();
+    }
+
+  }
 
 });
